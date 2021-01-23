267794bdd60397b78958c0a9db2320f853316304
ferregomezjp <ferregomezjp@gmail.com>
2021-01-23 11:32:41 -0500

    first commit

---
+++ build/precache-manifest.42992cc204dc0091142e07456ccc7995.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9abdd26eee024e9419b49febdd445883",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1db09d018620016c1b36",
+    "url": "./static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "0ee024718f445e390801",
+    "url": "./static/css/main.0a0f393b.chunk.css"
+  },
+  {
+    "revision": "1db09d018620016c1b36",
+    "url": "./static/js/2.2c409910.chunk.js"
+  },
+  {
+    "revision": "959444e7bc5405626d18ae4555c095a3",
+    "url": "./static/js/2.2c409910.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ee024718f445e390801",
+    "url": "./static/js/main.1fdc59c6.chunk.js"
+  },
+  {
+    "revision": "cdc766d8dfe69695d98d",
+    "url": "./static/js/runtime-main.bce8885d.js"
+  },
+  {
+    "revision": "e3741d9b2ef1808d3e4cab1a1ab29ca8",
+    "url": "./static/media/cancel.e3741d9b.svg"
+  },
+  {
+    "revision": "4b4cf493f88fb594bc1a7072e5b52902",
+    "url": "./static/media/eye.4b4cf493.svg"
+  },
+  {
+    "revision": "bc576d921a4eb15f1b27068fc1dc021a",
+    "url": "./static/media/printer.bc576d92.svg"
+  }
+]);
